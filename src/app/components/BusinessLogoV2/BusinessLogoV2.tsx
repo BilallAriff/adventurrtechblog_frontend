7a7d680f03af127abc7881653b4e51ef08aad597
Bila Arif <bilalarifhussain@gmail.com>
2023-12-17 12:19:05 +0500

    blog post styling complete

--- src/app/components/BusinessLogoV2/BusinessLogoV2.tsx
+++ src/app/components/BusinessLogoV2/BusinessLogoV2.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 const BusinessLogoV2 = () => {
+  const router = useRouter();
+
   return (
-    <Box>
+    <Box
+      component={"span"}
+      onClick={() => router.push("/")}
+      sx={{ cursor: "pointer" }}
+    >
       <Typography>Adventurr.tech</Typography>
     </Box>
   );
--- src/app/post/[postslug]/page.jsx
+++ src/app/post/[postslug]/page.jsx
@@ -3,6 +3,7 @@ import Container from "@/app/components/Container/Container";
 import GlobalLoadingUI from "@/app/components/GlobalLoadingUI/GobalLoadingUI";
 import HeaderComponent from "@/app/components/HeaderComponent/HeaderComponent";
 import PostHeading from "@/app/components/PostHeading/PostHeading";
+import SectionHeading from "@/app/components/SectionHeading/SectionHeading";
 import { useGetBlogBySlugQuery } from "@/app/redux/services/blogs";
 import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
 import { useParams, useRouter } from "next/navigation";
@@ -33,34 +34,18 @@ const BlogPost = () => {
           />
         </Container>
       </Grid>
-      {/* <Grid md={12}>
-        <Box>
-          <img
-            width={"100%"}
-            src={`${process.env.NEXT_PUBLIC_STRAPI_BASE_ASSET_URL}${data?.data?.[0]?.attributes?.thumbnail?.data?.attributes?.formats?.large?.url}`}
-          />
-        </Box>
-      </Grid> */}
       <Grid md={12}>
-        <Typography textAlign={"center"} my={3} variant="h3">
-          {data?.data?.[0]?.attributes?.title}
-        </Typography>
+        <Container>
+          <Box my={13}>
+            <SectionHeading text={data?.data?.[0]?.attributes?.title} />
+          </Box>
+        </Container>
       </Grid>
-      {/* <button
-        onClick={() =>
-          console.log({
-            params,
-            data,
-            blog: data?.data?.[0]?.attributes?.blogBody,
-          })
-        }
-      >
-        ddd
-      </button> */}
-      {/* {`${params.postslug}`} */}
       <Grid item md={3}></Grid>
       <Grid item md={6}>
-        <Markdown>{data?.data?.[0]?.attributes?.blogBody}</Markdown>
+        <Box className="blog-post-section">
+          <Markdown>{data?.data?.[0]?.attributes?.blogBody}</Markdown>
+        </Box>
       </Grid>
       <Grid item md={3}></Grid>
     </Grid>
