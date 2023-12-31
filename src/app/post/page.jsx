c4a1e4bf042e369137263df57c9893b941e6745a
Bila Arif <bilalarifhussain@gmail.com>
2023-12-31 12:26:33 +0500

    version 2 complete

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,7 +1,9 @@
+"use client";
 import { Grid } from "@mui/material";
 import React from "react";
 
-const page = ({ children }) => {
+const page = (props: any) => {
+  const { children } = props;
   return <>{children}</>;
 };
 
